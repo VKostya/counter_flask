04d4229b257a5fd1a8c9d667ad7b9b3f61e2eb95
Konstantin Voronovich <kvoronovich@emphasoft.com>
2024-11-14 00:04:10 +0300

    global updates

--- app.py
+++ app.py
@@ -1,41 +1,52 @@
-from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
+import os
+import psycopg2
+from flask import Flask, render_template, Response, request
 from datetime import datetime
+from db import get_db_connection, init_table_in_db
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://flask:flask@db:5432/counterdb"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
-
-
-class Counter(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    client_info = db.Column(db.String, nullable=True)
 
 
 @app.route("/")
 def home():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    date = datetime.now()
     user_agent = request.headers.get("User-Agent")
-    counter_record = Counter(client_info=user_agent)
-
-    db.session.add(counter_record)
-    db.session.commit()
-
-    return jsonify(
-        {
-            "id": counter_record.id,
-            "timestamp": counter_record.timestamp,
-            "client_info": counter_record.client_info,
-        }
-    )
+    try:
+        cur.execute(
+            "INSERT INTO counter (date, user_agent) VALUES (%s, %s)", (date, user_agent)
+        )
+        conn.commit()
+    except:
+        return Response("{'status':'500'}", status=500, mimetype="application/json")
+    cur.close()
+    conn.close()
+    return {"status": "200"}
 
 
 @app.route("/count", methods=["GET"])
 def get_count():
-    count = Counter.query.count()
-    return jsonify({"count": count})
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("select count(*) from counter")
+    resp = cur.fetchone()
+    cur.close()
+    conn.close()
+    return {"count": resp}
+
+
+@app.route("/list", methods=["GET"])
+def get_list():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("select * from counter;")
+    resp = cur.fetchall()
+    cur.close()
+    conn.close()
+    return {"resp": resp}
 
 
 if __name__ == "__main__":
+    init_table_in_db()
     app.run(debug=True, host="0.0.0.0", port=5000)
---
+++ db.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="lab_5-db-1", database="counterdb", user="flask", password="flask"
+    )
+    return conn
+
+
+def init_table_in_db():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS counter (id serial PRIMARY KEY, date varchar, user_agent varchar);"
+        )
+        print("table inited")
+        conn.commit()
+    except:
+        print("table init error")
+    cur.close()
+    conn.close()
